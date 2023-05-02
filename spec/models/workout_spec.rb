f5498130421332c8a5f87c16ba5be65f90d6438b
Merdan D <merdan.durdiyev@gmail.com>
2023-05-03 00:33:42 +0500

    Updates workout unit tests

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -3,46 +3,44 @@ require 'rails_helper'
 RSpec.describe Workout, type: :model do
 
   let(:category) { create(:category) }
+  let(:valid_workout) { create(:workout) }
 
-  it "successfully creates a workout" do
-    # workout = Workout.create(name: "Workout-1", level: "easy", category: category)
-    workout = create(:workout)
-
-    expect(workout).to be_valid
-    expect(Workout.count).to eq(1)
+  it "successfully creates a workout with valid params" do
+    expect{ valid_workout }.to change{ Workout.count }.by(1)
+    expect(valid_workout).to be_valid
   end
 
   it "fails to create a workout with name less than 3 chars" do
-    workout = Workout.create(name: "WK", level: "easy", category: category)
+    invalid_workout = build(:workout, name: "WK")
 
-    expect(workout).to_not be_valid
-    expect(Workout.count).to eq(0)
-    expect(workout.errors.messages[:name]).to include("is too short (minimum is 3 characters)")
+    expect{ invalid_workout }.to change{ Workout.count }.by(0)
+    expect(invalid_workout).to_not be_valid
+    expect(invalid_workout.errors.messages[:name]).to include("is too short (minimum is 3 characters)")
   end
 
   it "fails to create a workout without a name" do
-    workout = Workout.create(level: "easy", category: category)
+    invalid_workout = build(:workout, name: nil)
 
-    expect(workout).to_not be_valid
-    expect(Workout.count).to eq(0)
-    expect(workout.errors.messages[:name]).to include("can't be blank")
-    expect(workout.errors.messages[:name]).to include("is too short (minimum is 3 characters)")
+    expect{ invalid_workout }.to change{ Workout.count }.by(0)
+    expect(invalid_workout).to_not be_valid
+    expect(invalid_workout.errors.messages[:name]).to include("can't be blank")
+    expect(invalid_workout.errors.messages[:name]).to include("is too short (minimum is 3 characters)")
   end
 
   it "fails to create a workout without a level" do
-    workout = Workout.create(name: "Workout-1", category: category)
+    invalid_workout = build(:workout, level: nil)
 
-    expect(workout).to_not be_valid
-    expect(Workout.count).to eq(0)
-    expect(workout.errors.messages[:level]).to include("can't be blank")
+    expect{ invalid_workout }.to change{ Workout.count }.by(0)
+    expect(invalid_workout).to_not be_valid
+    expect(invalid_workout.errors.messages[:level]).to include("can't be blank")
   end
 
   it "fails to create a workout without a category provided" do
-    workout = Workout.create(name: "Workout-1", level: "easy")
+    invalid_workout = build(:workout, category: nil)
 
-    expect(workout).to_not be_valid
-    expect(Workout.count).to eq(0)
-    expect(workout.errors.messages[:category_id]).to include("can't be blank")
+    expect{ invalid_workout }.to change{ Workout.count }.by(0)
+    expect(invalid_workout).to_not be_valid
+    expect(invalid_workout.errors.messages[:category_id]).to include("can't be blank")
   end
 
 end
