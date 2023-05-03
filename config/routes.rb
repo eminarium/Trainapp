4c87c29fb256d24dd1b3f3e096c5609c7bc392b2
Merdan D <merdan.durdiyev@gmail.com>
2023-05-03 16:58:43 +0500

    Adds Login/Logout buttons on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
+  # Defines the root path route ("/")
+  root "categories#index"
+
   devise_for :users
   resources :categories
   resources :exercises
   resources :workouts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "categories#index"
 end
