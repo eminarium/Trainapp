3a9da2493059f454ef8557af0de54961caeb24c5
Merdan D <merdan.durdiyev@gmail.com>
2023-05-03 09:45:43 +0500

    Updates factories to have longer names

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,25 +1,25 @@
 FactoryBot.define do
   factory :workout_exercise do
-    workout { nil }
-    exercise { nil }
-    repetitions { 1 }
+    workout { create(:workout) }
+    exercise { create(:exercise, workout: workout) }
+    repetitions { 3 }
   end
 
   factory :workout do
-    name { Faker::Lorem.words }
+    name { Faker::Lorem.words.join(" ") }
     category { create(:category) }
     level { Workout.levels.keys.sample }
     description { Faker::Lorem.sentence }
   end
 
   factory :exercise do
-    name { Faker::Lorem.words }
+    name { Faker::Lorem.words.join(" ") }
     category { create(:category) }
     description { Faker::Lorem.sentence }
   end
 
   factory(:category) do
-    name { Faker::Lorem.word }
+    name { Faker::Lorem.words.join(" ") }
     description { Faker::Lorem.sentence }
   end
 end
